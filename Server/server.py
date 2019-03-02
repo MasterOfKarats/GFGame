760187b3b8c08111fec0e556024d0c1cdaf602fd
Admin <uladzimi@buffalo.edu>
2019-03-02 15:24:50 -0500

    The server is up, displays everything that is in the index.html file. Although you won't be able to run it from your computers for now.

--- Server/server.py
+++ Server/server.py
@@ -1,8 +1,22 @@
-import flask
+from flask import Flask, render_template
 import FrontEnd
+app = Flask(__name__, template_folder='D:\ScalaProjects\GoFishClone\FrontEnd')
 
-app = flask(_name_)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return 'FrontEnd/htmlfile.html'
+    return render_template('index.html')
+
+
+@app.route("/cards.js")
+def cards():
+    return render_template('Cards.js')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+#app = flask(_name_)
+#host='0.0.0.0', port=8080,
+
