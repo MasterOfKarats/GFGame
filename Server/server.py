51b321be1739d5d3d3d9d4afb0d5dfdf73cd1e28
Admin <uladzimi@buffalo.edu>
2019-03-25 10:04:56 -0400

    database

--- Server/server.py
+++ Server/server.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__, template_folder='D:\ScalaProjects\GoFishClone\FrontEnd')
 
 
 @app.route("/")
 def homepage():
-    return app.send_static_file('index.html')
+    return render_template('index.html')
 
 
 @app.route("/cards.js")
@@ -18,4 +18,4 @@ if __name__ == "__main__":
 
 #app = flask(_name_)
 #host='0.0.0.0', port=8080,
-#, template_folder=' '
+#,
